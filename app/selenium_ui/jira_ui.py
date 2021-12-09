93be6c63ce1ac08338114b7a064fab0e011fa9ec
Andre Fernandes Rodrigues <andre.fernandes.rodrigues@idera.com>
2021-12-09 15:59:56 +0000

    XPORTER_DC_APPROVAL_2021

--- app/selenium_ui/jira_ui.py
+++ app/selenium_ui/jira_ui.py
@@ -6,7 +6,6 @@ from extension.jira import extension_ui  # noqa F401
 def test_0_selenium_a_login(jira_webdriver, jira_datasets, jira_screen_shots):
     modules.login(jira_webdriver, jira_datasets)
 
-
 def test_1_selenium_browse_projects_list(jira_webdriver, jira_datasets, jira_screen_shots):
     modules.browse_projects_list(jira_webdriver, jira_datasets)
 
@@ -60,9 +59,14 @@ Add custom actions anywhere between login and log out action. Move this to a dif
 Write your custom selenium scripts in `app/extension/jira/extension_ui.py`.
 Refer to `app/selenium_ui/jira/modules.py` for examples.
 """
-# def test_1_selenium_custom_action(jira_webdriver, jira_datasets, jira_screen_shots):
-#     extension_ui.app_specific_action(jira_webdriver, jira_datasets)
+def test_1_selenium_xporter_single_export_dialog(jira_webdriver, jira_datasets, jira_screen_shots):
+    extension_ui.app_single_action(jira_webdriver, jira_datasets)
 
+def test_1_selenium_xporter_bulk_export_dialog(jira_webdriver, jira_datasets, jira_screen_shots):
+    extension_ui.app_bulk_action(jira_webdriver, jira_datasets)
+
+def test_1_selenium_xporter_xlsx_current_fields_export(jira_webdriver, jira_datasets, jira_screen_shots):
+    extension_ui.app_xlsx_current_fields_action(jira_webdriver, jira_datasets)
 
 # this action should be the last one
 def test_2_selenium_z_log_out(jira_webdriver, jira_datasets, jira_screen_shots):
